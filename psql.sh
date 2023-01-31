b2c14aa5d82396486f122ad2dd4d1c657bff5dd9
pius ssekweyama <ssekweyamapius@gmail.com>
2023-01-31 10:34:04 +0300

    postgres

---
+++ psql.sh
@@ -0,0 +1,1 @@
+psql --host=localhost --port=5432 --username=postgres --password
--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE DATABASE vet_clinic;
+USE vet_clinic;
+CREATE TABLE animals (id BIGSERIAL NOT NULL PRIMARY KEY,name VARCHAR(255) NOT NULL,date_of_birth DATE NOT NULL,escape_attemps INT NOT NULL,neutered BOOLEAN NOT NULL,weight_kg DECIMAL(5,2) NOT NULL);
