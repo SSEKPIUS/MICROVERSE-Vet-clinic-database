74ceef6aded552719b571aca03b237ec0a57a636
pius ssekweyama <ssekweyamapius@gmail.com>
2023-01-31 10:43:50 +0300

    readme

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
--- FINd all animals whose name ends in "mon".
+-- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 --List the name of all animals born between 2016 and 2019
