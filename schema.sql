a38ccd76adeca51c475549f3c386727f1582f3d2
pius ssekweyama <ssekweyamapius@gmail.com>
2023-02-01 14:00:19 +0300

    excercises

--- schema.sql
+++ schema.sql
@@ -2,3 +2,6 @@
 CREATE DATABASE vet_clinic;
 USE vet_clinic;
 CREATE TABLE animals (id BIGSERIAL NOT NULL PRIMARY KEY,name VARCHAR(255) NOT NULL,date_of_birth DATE NOT NULL,escape_attemps INT NOT NULL,neutered BOOLEAN NOT NULL,weight_kg DECIMAL(5,2) NOT NULL);
+
+/* Vet clinic database: query and update animals table */
+ALTER TABLE animals ADD COLUMN species varchar(255);
